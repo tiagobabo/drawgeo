40ccf6308f984a9a4998d8e48762890adb867388
Felipe Schmitt <fsschmitt@gmail.com>
2012-05-21 02:48:13 +0100

    Pasta do servidor e base do website promocional

---
+++ Server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = 'e369d1e057cac1f4d9271dc8bc1daec4e0fd8d80095a345f0c2f292f9841a821af265d0d946c207f915fbc8240f90780742eca5b4c59ede1a4c71005b75c68d8'
