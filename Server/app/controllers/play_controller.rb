c3c0a7951ef4f82f06ec73acc86afd0cba1c368b
Felipe Schmitt <fsschmitt@gmail.com>
2012-05-25 11:44:55 +0100

    User can't guess it's own word.

--- Server/app/controllers/play_controller.rb
+++ Server/app/controllers/play_controller.rb
@@ -8,25 +8,31 @@ class PlayController < ApplicationController
 		@draw_user = DrawUser.where("id_draw = ? AND id_user= ?", params[:draw_id], params[:id])
 
 		if(@draw_user.empty?)
-			if(@guess == @word.word && @player.id != @author.id)
-				@draw_user = DrawUser.new(:id_draw => @draw.id, :id_user => @player.id)
-				if @draw_user.save
-					@player.update_attribute(:num_done, @player.num_done + 1)
-					@author.update_attribute(:num_success, @author.num_success + 1)
-					@player.update_attribute(:piggies, @player.piggies + (@word.difficulty * 2))
-					@author.update_attribute(:piggies, @author.piggies + (@word.difficulty))
-				end
+			if(@player.id != @author.id)
+				if(@guess == @word.word)
+					@draw_user = DrawUser.new(:id_draw => @draw.id, :id_user => @player.id)
+					if @draw_user.save
+						@player.update_attribute(:num_done, @player.num_done + 1)
+						@author.update_attribute(:num_success, @author.num_success + 1)
+						@player.update_attribute(:piggies, @player.piggies + (@word.difficulty * 2))
+						@author.update_attribute(:piggies, @author.piggies + (@word.difficulty))
+					end
 
-				if(@draw.challenge)
-					@player.update_attribute(:piggies, @player.piggies + 1)
-					@author.update_attribute(:piggies, @author.piggies + 1)
+					if(@draw.challenge)
+						@player.update_attribute(:piggies, @player.piggies + 1)
+						@author.update_attribute(:piggies, @author.piggies + 1)
+					end
+					respond_to do |format|
+				      format.json { render :json => {:status => "Ok", :word => @word}.to_json }
+				    end
+				else
+					respond_to do |format|
+				      format.json { render :json => {:status => "Incorrect guess."}.to_json }
+				    end
 				end
-				respond_to do |format|
-			      format.json { render :json => {:status => "Ok", :word => @word}.to_json }
-			    end
 			else
 				respond_to do |format|
-			      format.json { render :json => {:status => "Incorrect"}.to_json }
+			      format.json { render :json => {:status => "You can't guess your own word."}.to_json }
 			    end
 			end
 		else
