136e0bfc820caf5174fc726b39b3090f75a3a5ba
Jose Pedro Marques <ze.p.smarques@gmail.com>
2012-05-29 18:42:16 +0100

    Ja poe os espacos dinamicamente e utiliza as letras. Tem um pequeno bug quando se apaga uma letra no meio e se continua a adicionar letras, fazendo override as letras a seguir

--- Android/src/pt/drawgeo/canvas/ReplayCanvasActivity.java
+++ Android/src/pt/drawgeo/canvas/ReplayCanvasActivity.java
@@ -7,18 +7,31 @@ import pt.drawgeo.utility.Configurations;
 import pt.drawgeo.utility.Connection;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.main.R;
 
 public class ReplayCanvasActivity extends Activity {
 	
 	private  DrawingPanel drawView; 
+	private int lastLetterPos = 0;
+	private String word = null;
+	EditText[] guessLetters =  null;
+	EditText[] usedLetters =  null;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,14 +68,21 @@ public class ReplayCanvasActivity extends Activity {
 			String ysString = draw.getString("drawy");
 			drawView = new DrawingPanel(this,colorsString,xsString,ysString);
 		    canvas.addView(drawView);
-        
+		    word = info.getString("word");
+		    TextView tv = (TextView) findViewById(R.id.title);
+		    tv.setText(word);
+		    addEnoughSquares(word.length());
 	        JSONArray guess = info.getJSONArray("guess");
 			EditText[] letters = new EditText[guess.length()];
 			
 			putLetters(letters);
 			
-			for(int i = 0; i <guess.length() ;i++)
-				letters[i].setText(guess.getString(i));
+			for(int i = 0; i <guess.length() ;i++){
+				String l = guess.getString(i);
+				letters[i].setText(l);
+				letters[i].setClickable(true);
+				letters[i].setOnClickListener(new LetterClickListener(letters[i]));
+			}
 			drawView.replay();
     	}
     	catch (Exception e){
@@ -71,7 +91,37 @@ public class ReplayCanvasActivity extends Activity {
         
     }
     
-    private void putLetters(EditText[] letters) {
+    private void addEnoughSquares(int length) {
+    	guessLetters =   new EditText[length];
+    	usedLetters =   new EditText[length];
+    	RelativeLayout guessLayout = (RelativeLayout) findViewById(R.id.guessLayout);
+    	for(int i = 1; i <= length; i++){
+	    	EditText et = new EditText(this);
+	    	et.setId(i);
+	    	
+	    	
+	    	RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams( LayoutParams.WRAP_CONTENT, 40 );
+	        if( i != 1 ){
+	            params.addRule(RelativeLayout.RIGHT_OF, i-1 );
+	            params.addRule(RelativeLayout.ALIGN_PARENT_TOP,1);
+	        }
+	        
+	        et.setLayoutParams(params);
+	        et.setGravity(Gravity.CENTER_VERTICAL| Gravity.CENTER_VERTICAL);
+	        et.setEms(10);
+	        et.setFocusable(false);
+	        et.setWidth(35);
+	        et.setClickable(true);
+	        et.setOnClickListener(new GuessClickListener());
+	        et.setText("");
+	    	guessLayout.addView(et);
+	    	
+	    	guessLetters[i-1] = et;
+    	}
+		
+	}
+
+	private void putLetters(EditText[] letters) {
 		
 		letters[0] =  (EditText) findViewById(R.id.char0);
 		letters[1] =  (EditText) findViewById(R.id.char1);
@@ -91,5 +141,46 @@ public class ReplayCanvasActivity extends Activity {
 		letters[15] =  (EditText) findViewById(R.id.char15);
 		
 	}
+    
+    private class LetterClickListener implements View.OnClickListener
+    {
+    	
+    	EditText et = null;
+    	public LetterClickListener(EditText et){
+    		this.et = et;
+    	}
+		public void onClick(View v) {
+			
+			if(lastLetterPos<guessLetters.length ){
+				usedLetters[lastLetterPos] = et;
+				guessLetters[lastLetterPos++].setText(et.getText());
+				et.setVisibility(4);
+				et.setClickable(false);
+				
+				
+			}
+			
+			
+		}
+	}
+    private class GuessClickListener implements View.OnClickListener
+    {
+    	
+    	
+    	
+		public void onClick(View v) {
+			
+			int id = v.getId()-1;
+			usedLetters[id].setVisibility(0);
+			usedLetters[id].setClickable(true);
+			guessLetters[id].setText("");
+			lastLetterPos = Math.min(lastLetterPos, id);
+			
+			
+			
+		}
+	}
+
+	
     
 }
